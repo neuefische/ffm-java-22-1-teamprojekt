25d3bb469d4f9673b60207b6387addf2dc6b6a20
voelkldennis <voelkldennis@googlemail.com>
2022-10-25 10:57:00 +0200

    Change App.tsx by xtreams

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 import Meals from "./Meals/Meals";
+import EmployeeOverview from "./employee/EmployeeOverview";
+
 export default function App() {
     return <>
-        Willkommen bei HackDonald's - Ich liebe es!
+       <h1>Willkommen bei HackDonald's - Ich liebe es!</h1>
         <Meals/>
+        <EmployeeOverview />
     </>
 }
