10ad22d356b5272b6fddde915ae80ee303c59311
asem259 <alghabra.asem@gmail.com>
2022-10-25 17:22:49 +0200

    add frontend PlacementPage, placementModel and Placement Components.

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/Placement.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/Placement.java
@@ -1,25 +1,4 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.placement;
 
-
-
-
-// TODO:
-// Placement Repo (): HashMap ()
-// TODO:
-// Service Unit Test
-// get All
-// TODO:
-// Post new Placement
-//  Service
-
-// Controller Integration Test
-// getAll
-// Placement Controller
-
-// Placements
-
-
-
-
 public record Placement(String id, int placementNr) {
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Meals from "./Meals/Meals";
 import EmployeeOverview from "./employee/EmployeeOverview";
+import PlacementPage from "./Placement/PlacementPage";
 
 export default function App() {
     return <>
         <h1>Willkommen bei HackDonald's - Ich liebe es!</h1>
         <Meals/>
         <EmployeeOverview />
+        <PlacementPage/>
     </>
 }
