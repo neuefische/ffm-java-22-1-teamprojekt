52bb4da5cc9bc421e6be0edbe701c22cb86fa26c
asem259 <alghabra.asem@gmail.com>
2022-10-25 10:24:20 +0200

    create Placement Record

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/Placement.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/Placement.java
@@ -1,6 +1,25 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.placement;
 
-public record Placement() {
-    
 
+
+
+// TODO:
+// Placement Repo (): HashMap ()
+// TODO:
+// Service Unit Test
+// get All
+// TODO:
+// Post new Placement
+//  Service
+
+// Controller Integration Test
+// getAll
+// Placement Controller
+
+// Placements
+
+
+
+
+public record Placement(String id, int placementNr) {
 }
