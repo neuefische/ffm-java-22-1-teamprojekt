1d41cf6e05d84efcec3d8a5d952530d972fd8ba6
asem259 <alghabra.asem@gmail.com>
2022-10-25 15:34:46 +0200

    add getAllPlacement: unit test & integration test.

---
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementUnitTest.java
@@ -0,0 +1,47 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.placement;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class PlacementUnitTest {
+
+    private final PlacementRepo testRepo = mock(PlacementRepo.class);
+    private final PlacementService testService = new PlacementService(testRepo);
+
+    @Test
+    public void getAllPlacementsAtBeginning() {
+        when(testRepo.getAllPlacement()).thenReturn(new ArrayList<>());
+
+        List<Placement> result = testService.getAllPlacements();
+
+        assertTrue(result.isEmpty());
+        // assertEquals(0,result.size());
+    }
+
+    @Test
+    public void getAllPlacementsWithExistPlacements() {
+        List<Placement> placements = new ArrayList<>(List.of(
+                new Placement(UUID.randomUUID().toString(), 1),
+                new Placement(UUID.randomUUID().toString(), 2),
+                new Placement(UUID.randomUUID().toString(), 3),
+                new Placement(UUID.randomUUID().toString(), 4),
+                new Placement(UUID.randomUUID().toString(), 5)
+        ));
+
+        when(testRepo.getAllPlacement()).thenReturn(placements);
+
+        List<Placement> result = testService.getAllPlacements();
+
+        assertArrayEquals(placements.toArray(), result.toArray());
+    }
+}
+
