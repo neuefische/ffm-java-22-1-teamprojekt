5fe4f2472b725c65341c3e11c728bdfb8c3397c8
Iryna <pshonka@gmail.com>
2022-10-25 16:49:57 +0200

    refactor meal record

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/meals/Meal.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/meals/Meal.java
@@ -1,4 +1,12 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.meals;
 
 public record Meal(String id, String name) {
+
+    Meal(String name) {
+        this(null, name);
+    }
+
+    public Meal withId(String id) {
+        return new Meal(id, this.name);
+    }
 }
