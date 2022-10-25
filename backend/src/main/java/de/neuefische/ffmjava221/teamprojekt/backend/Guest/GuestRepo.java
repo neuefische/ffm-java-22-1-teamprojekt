8ca26e380be84720db0e889e6f65e3b39a0e9e31
anagalit <ana.galit1@gmail.com>
2022-10-25 20:12:46 +0200

    Adjust method getGuestList in GuestRepo

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/Guest/GuestRepo.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/Guest/GuestRepo.java
@@ -2,23 +2,21 @@ package de.neuefische.ffmjava221.teamprojekt.backend.Guest;
 
 import org.springframework.stereotype.Repository;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class GuestRepo {
 
-    private List<Guest> guests;
+    private List<Guest> guests = new ArrayList<>();
 
     public Guest addGuestData(Guest guest) {
         guests.add(guest);
         return guest;
-
     }
 
     public List<Guest> getGuestList() {
-        return new ArrayList<>();
+        return this.guests;
     }
 }
 
