0a19e2b42947f8c6887d3d61e1058f95d75af2e2
voelkldennis <voelkldennis@googlemail.com>
2022-10-25 10:56:40 +0200

    Initial Frontend: getRequest, and EmployeeModel usw.

---
+++ frontend/src/employee/EmployeeCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {EmployeeModel} from './EmployeeModel'
+
+type employeeProps = {
+    employee: EmployeeModel,
+}
+
+export default function EmployeeCard(props: employeeProps) {
+    return (<>
+        <li>Name: {props.employee.name} ID: {props.employee.id}</li>
+    </>
+    );
+}
