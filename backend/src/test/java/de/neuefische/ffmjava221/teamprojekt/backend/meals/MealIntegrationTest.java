22e311f7272a7d1ab58420a70ca5830e465e0e87
Iryna <pshonka@gmail.com>
2022-10-25 17:27:08 +0200

    backend:  add integrationtest for addMeal

--- backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/meals/MealIntegrationTest.java
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/meals/MealIntegrationTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,5 +24,30 @@ class MealIntegrationTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/meals"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void addMealWithoutIdAndReturnMeal() throws Exception  {
+        String body = mockMvc.perform(MockMvcRequestBuilders.post("/api/meals")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name": "Wurst"
+                            }
+                            """))
+                .andExpect(status().is(201))
+                .andReturn().getResponse().getContentAsString();
+
+        Meal meal = objectMapper.readValue(body, Meal.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/meals"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    [{
+                        "id": "<id>",
+                        "name": "Wurst"
+                    }]
+                    """.replace("<id>",meal.id())));
     }
 }
