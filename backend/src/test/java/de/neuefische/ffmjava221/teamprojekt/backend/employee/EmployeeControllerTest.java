37f8d57e3cbea7f1cd41a3390fb7e39d348f8fa7
voelkldennis <voelkldennis@googlemail.com>
2022-10-25 16:46:20 +0200

    delete commend test

--- backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeControllerTest.java
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeControllerTest.java
@@ -1,5 +1,4 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.employee;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,8 +9,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,7 +16,6 @@ class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    private ObjectMapper objectMapper;
 
 
     @DirtiesContext
@@ -48,20 +44,4 @@ class EmployeeControllerTest {
                                     """));
     }
 
-
-
-//    @Test
-//    void postEmployee() throws Exception{
-//        //GIVEN
-//        String expected = objectMapper.writeValueAsString(List.of(
-//                new EmployeeCard("1","Marc"),
-//                new EmployeeCard("2","Chris"),
-//                new EmployeeCard("3","Dennis")));
-//        //WHEN
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/employees"))
-//        //THEN
-//                .andExpect(status().isOk())
-//                .andExpect(content().json(expected));
-//    }
 }
