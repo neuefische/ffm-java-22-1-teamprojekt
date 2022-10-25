6e2e76cbc1630a17ce3a779cf42e833efb474b17
Iryna <pshonka@gmail.com>
2022-10-25 09:59:55 +0200

    add import from Meals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import Meals from "./Meals/Meals";
 export default function App() {
     return <>
         Willkommen bei HackDonald's - Ich liebe es!
