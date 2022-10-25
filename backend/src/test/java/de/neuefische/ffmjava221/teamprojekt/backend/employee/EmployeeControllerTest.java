2ff9de3218955e384e29de4dd44e508d95a9b0bb
voelkldennis <voelkldennis@googlemail.com>
2022-10-25 18:50:09 +0200

    add tests, add Post Method addEmployeeTEST problems

--- backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeControllerTest.java
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeControllerTest.java
@@ -1,11 +1,14 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.employee;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -17,31 +20,65 @@ class EmployeeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @DirtiesContext
     @Test
-    void getProductBananaFromProduct() throws Exception{
+    void getAllEmployeesTEST() throws Exception{
 
         //GIVEN & WHEN
         mockMvc.perform(MockMvcRequestBuilders.get("/api/employees"))
                 //THEN
                 .andExpect(status().isOk())
-                .andExpect(content().json("""
-                                    [
+                .andExpect(content().json("[]"));
+    }
+
+//    @DirtiesContext
+//    @Test
+//    void postEmployeeTEST() throws Exception{
+//        //GIVEN
+//        String expected = objectMapper.writeValueAsString(List.of(
+//                new EmployeeCard(EmployeeUtils.generateUUID().toString(), "horst")
+//        ));
+//
+//        MockMvcRequestBuilders.post("/api/employees"){
+//
+//        };
+//
+//        //WHEN
+//        mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/"))
+//                //THEN
+//                .andExpect(status().isOk())
+//                .andExpect(content().json(expected));
+//    }
+
+    @DirtiesContext
+    @Test
+    void addEmployeeReturnsEmplyeeWithID() throws Exception {
+        //given
+        String content = mockMvc.perform(MockMvcRequestBuilders.post("/api/employees")
+
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
                                         {
-                                            "id": "1",
-                                            "name": "Marc"
-                                        },
-                                        {
-                                            "id": "2",
-                                            "name": "Chris"
-                                        },
-                                        {
-                                            "id": "3",
-                                            "name": "Dennis"
+                                            "name": "Hasi"
                                         }
-                                    ]
-                                    """));
+                                        """))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        Employee employee = objectMapper.readValue(content, Employee.class);
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/employees"))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    [{
+                        "id": "<id>",
+                        "name": "Hasi"
+                    }]
+                    """.replace("<id>", employee.id())));
     }
 
 }
