0763485ddd648b5aabc4b7421a4144492633eb29
voelkldennis <voelkldennis@googlemail.com>
2022-10-25 10:03:57 +0200

    Initial Commit create employee

---
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeController.java
@@ -0,0 +1,23 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    List<EmployeeCard> getAllEmployees() {
+        return employeeService.getAll();
+    }
+}
