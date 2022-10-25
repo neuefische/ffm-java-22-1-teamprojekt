f93f744ff7b8e05cbd35bc61ad392ffce6622fe0
asem259 <alghabra.asem@gmail.com>
2022-10-25 16:04:27 +0200

    add getAllPlacement: integration test.

---
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementIntegrationTest.java
@@ -0,0 +1,42 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.placement;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PlacementIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext
+    public void getAllPlacements() throws Exception {
+        String content = mvc.perform(MockMvcRequestBuilders.get("/api/placements"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        List<Placement> response = objectMapper.readValue(content, new TypeReference<>() {
+        });
+
+        assertTrue(response.isEmpty());
+    }
+
+}
