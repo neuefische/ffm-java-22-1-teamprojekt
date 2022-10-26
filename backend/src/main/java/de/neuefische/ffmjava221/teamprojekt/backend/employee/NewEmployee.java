4b39c2c744182aa4850d591cc99657e789285d7c
voelkldennis <voelkldennis@googlemail.com>
2022-10-26 15:47:59 +0200

    change Upper case ID to lower case Id inside newEmployee and delete doubled method withID

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/NewEmployee.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/NewEmployee.java
@@ -3,7 +3,7 @@ package de.neuefische.ffmjava221.teamprojekt.backend.employee;
 public record NewEmployee(
         String name
 ) {
-    public Employee withID(String id) {
+    public Employee withId(String id) {
         return new Employee(id, name);
     }
 }
