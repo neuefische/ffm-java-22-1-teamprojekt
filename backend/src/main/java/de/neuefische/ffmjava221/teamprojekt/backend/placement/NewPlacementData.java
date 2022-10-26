f7ed940c3af47e5b59f578a9befdd91ec21cf480
asem259 <alghabra.asem@gmail.com>
2022-10-26 12:27:26 +0200

    add new Placement to the placement repository.

---
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/NewPlacementData.java
@@ -0,0 +1,10 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.placement;
+
+import lombok.Data;
+
+@Data
+public class NewPlacementData {
+    private int placementNr;
+    private int totalSeats;
+
+}
