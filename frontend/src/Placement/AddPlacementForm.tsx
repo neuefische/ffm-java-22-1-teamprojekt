5477caf23791506ad64861cbf0406596e3f21e6a
mykola <bobirets@gmail.com>
2022-10-26 18:01:53 +0200

    add new Placement to Frontend

--- frontend/src/Placement/AddPlacementForm.tsx
+++ frontend/src/Placement/AddPlacementForm.tsx
@@ -1,19 +1,54 @@
-import React, {ChangeEvent, FormEvent} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import axios from 'axios';
+import PlacementModel from "./PlacementModel";
 
-function AddPlacementForm() {
-    const handleSubmit=(evt: FormEvent<HTMLFormElement>)=>{
+type AddPlacementFormProps={
+    fetchAll:()=>void;
+}
+
+function AddPlacementForm(props:AddPlacementFormProps) {
+    const [tableNumber, setTableNumber] = useState<string>('')
+    const [totalSeats, setTotalSeats] = useState<string>('')
+
+
+    const postNewPlacement = (newPlacement: Omit<PlacementModel, 'id'>) => {
+        axios.post("api/placements", newPlacement)
+            .then(()=>{
+                props.fetchAll();
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+
     }
 
-    const handleChange=(evt: ChangeEvent<HTMLInputElement>)=>{
+// ToDo : Sort placements? - backend duplicate Nr!!.
+    const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        console.log('[Submit Form] => ', 'Table number: ' + tableNumber, ' Total seats: ' + totalSeats)
+        postNewPlacement({placementNr: parseInt(tableNumber), totalSeats: parseInt(totalSeats)})
+    }
+
+    const handleTableNumberChange = (evt: ChangeEvent<HTMLInputElement>) => {
+        setTableNumber(evt.target.value)
+    }
+    const handleTotalSeatsChange = (evt: ChangeEvent<HTMLInputElement>) => {
+        setTotalSeats(evt.target.value)
     }
 
     return (
-            <form onSubmit={handleSubmit}>
-                <input type='text' id='placement-Nr' onChange={handleChange}/>
-                <input type='text' id='total-seats' onChange={handleChange}/>
-                <button>Add</button>
-            </form>
-       
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='placement-Nr'>Table Nr.: </label>
+                <input type='text' id='placement-Nr' onChange={handleTableNumberChange} value={tableNumber}/>
+            </div>
+            <div>
+                <label htmlFor='total-seats'>Total Seats: </label>
+                <input type='text' id='total-seats' onChange={handleTotalSeatsChange} value={totalSeats}/>
+            </div>
+            <button>Add</button>
+        </form>
+
     );
 }
 
