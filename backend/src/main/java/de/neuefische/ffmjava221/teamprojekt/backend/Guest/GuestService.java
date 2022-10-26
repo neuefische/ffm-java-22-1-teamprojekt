25e59ea3a5402cd7bab08be72e402f12a939aee7
anagalit <ana.galit1@gmail.com>
2022-10-26 14:18:59 +0200

    Add uuid to addGuestMethod

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/Guest/GuestService.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/Guest/GuestService.java
@@ -2,7 +2,9 @@ package de.neuefische.ffmjava221.teamprojekt.backend.Guest;
 
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class GuestService {
@@ -13,7 +15,10 @@ public class GuestService {
         this.guestRepo = guestRepo;
     }
 
-    public Guest addGuestData(Guest guest) {
+    public Guest addGuestData(NewGuest newGuest) {
+        String uuid = UUID.randomUUID().toString();
+//        System.out.println(uuid);
+        Guest guest = new Guest(uuid, newGuest.firstName(), newGuest.lastName(), newGuest.email(), newGuest.password(), newGuest.confirmPassword());
         return guestRepo.addGuestData(guest);
     }
 
