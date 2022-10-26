7b751c7ee0d83acc41df85fa4ec85afe2e115e82
asem259 <alghabra.asem@gmail.com>
2022-10-26 15:42:54 +0200

    add new Placement integration test.

--- backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementIntegrationTest.java
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementIntegrationTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -37,5 +38,28 @@ class PlacementIntegrationTest {
 
         assertTrue(response.isEmpty());
     }
+
+    @Test
+    @DirtiesContext
+    void postNewPlacement() throws Exception {
+        String requestPlacement = """
+                {
+                "placementNr":5,
+                "totalSeats":8
+                }
+                """;
+
+        String content = mvc.perform(MockMvcRequestBuilders.post("/api/placements").contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestPlacement))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        Placement newPlacement = objectMapper.readValue(content, Placement.class);
+
+        assertFalse(newPlacement.id().isEmpty());
+        assertEquals(5, newPlacement.placementNr());
+        assertEquals(8, newPlacement.totalSeats());
+    }
+
 
 }
