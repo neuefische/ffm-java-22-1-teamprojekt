17401908d6dedcc597fe8e08aee3b0611e24d783
asem259 <alghabra.asem@gmail.com>
2022-10-26 15:17:03 +0200

    add new Placement controller.

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementController.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementController.java
@@ -1,16 +1,12 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.placement;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequestMapping("/api/placements")
 @RestController
 public class PlacementController {
-
-
     private final PlacementService placementService;
 
     public PlacementController(PlacementService placementService) {
@@ -21,4 +17,10 @@ public class PlacementController {
     public List<Placement> getAllPlacement() {
         return placementService.getAllPlacements();
     }
+
+    @PostMapping()
+    public Placement addNewPlacement(@RequestBody NewPlacementData requestPlacement) {
+        return placementService.addNewPlacement(requestPlacement);
+    }
 }
+
