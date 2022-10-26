0e0f9f9bdfc061665f6cbf38ddde6c5f25e3f472
asem259 <alghabra.asem@gmail.com>
2022-10-26 12:27:26 +0200

    add new Placement to the placement repository.

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementRepo.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementRepo.java
@@ -2,17 +2,22 @@ package de.neuefische.ffmjava221.teamprojekt.backend.placement;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Repository
 public class PlacementRepo {
 
-    private final Map<String,Placement> placementMap = new HashMap<>();
+    private final Map<String, Placement> placementMap = new HashMap<>();
 
-    public List<Placement> getAllPlacement(){
-       return new ArrayList<>(placementMap.values());
+
+    public List<Placement> getAllPlacement() {
+        return new ArrayList<>(placementMap.values());
+    }
+
+    public Placement addNewPlacement(NewPlacementData newPlacementData) {
+        String newUuid = UUID.randomUUID().toString();
+        Placement newPlacement = new Placement(newUuid, newPlacementData.getPlacementNr(), newPlacementData.getTotalSeats());
+        placementMap.put(newPlacement.id(), newPlacement);
+        return newPlacement;
     }
 }
