8fff718731440405d4bc486f61b83b1aca5e67db
konypro <ktyker.kt@gmail.com>
2022-10-27 10:58:49 +0200

    Fix UUID Util

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementService.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementService.java
@@ -10,6 +10,7 @@ public class PlacementService {
 
     private final PlacementRepo placementRepo;
 
+
     public PlacementService(PlacementRepo placementRepo) {
         this.placementRepo = placementRepo;
     }
@@ -18,8 +19,17 @@ public class PlacementService {
         return placementRepo.getAllPlacement();
     }
 
-    public Placement addNewPlacement(NewPlacementData requestPlacement) {
-        return placementRepo.addNewPlacement(requestPlacement);
+    public Placement addNewPlacement(NewPlacementData newRequestPlacement) {
+
+        String newUuid = PlacementUtil.generateUuid();
+        int placementNr = newRequestPlacement.placementNr();
+        int totalSeats = newRequestPlacement.totalSeats();
+
+        Placement newPlacement = new Placement(newUuid, placementNr, totalSeats);
+        placementRepo.addNewPlacement(newPlacement);
+
+        return newPlacement;
     }
+
 
 }
---
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/PlacementUtil.java
@@ -0,0 +1,11 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.placement;
+
+import java.util.UUID;
+
+
+public class PlacementUtil {
+
+    public static String generateUuid() {
+        return UUID.randomUUID().toString();
+    }
+}
