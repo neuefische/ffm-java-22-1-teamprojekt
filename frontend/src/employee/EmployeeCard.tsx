36e3f39b9ca889e5d18808e7762528d164b43e13
voelkldennis <voelkldennis@googlemail.com>
2022-10-27 10:46:28 +0200

    add deleteEmployee Frontend

--- frontend/src/employee/EmployeeCard.tsx
+++ frontend/src/employee/EmployeeCard.tsx
@@ -3,10 +3,15 @@ import {EmployeeModel} from './EmployeeModel'
 
 type employeeProps = {
     employee: EmployeeModel,
+    deleteEmployee: (id: string) => void,
 }
 
+
 export default function EmployeeCard(props: employeeProps) {
+
     return <>
-        <li>Name: {props.employee.name} ID: {props.employee.id}</li>
+        <li>Name: {props.employee.name} ID: {props.employee.id}
+            <button onClick={() => props.deleteEmployee(props.employee.id)}>Delete Employee</button>
+        </li>
     </>
 }
