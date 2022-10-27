9b600f5e04d52af7c40b7878e231dacf9b1bde22
anagalit <ana.galit1@gmail.com>
2022-10-27 11:36:08 +0200

    Commit for add and get methods

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/GuestController.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/GuestController.java
@@ -1,5 +1,7 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.guest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
@@ -23,4 +25,21 @@ public class GuestController {
     List<Guest> getGuestList() {
         return guestService.getGuestList();
     }
+
+//// Put nur für Vorname und Nachname
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Guest> updateGuest(@PathVariable String id, @RequestBody Guest guest){
+//        if (guest.id().equals(id)) {
+//            int index = guestService.getIndexOfId(id);
+//            Guest newGuest = guestService.updateGuest(index, guest);
+//            boolean guestExists = (index>=0);
+//            return guestExists ? new ResponseEntity<>(newGuest,HttpStatus.OK) : new ResponseEntity<>(newGuest,HttpStatus.CREATED);
+//        } return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//    }
+
+    @PutMapping("{id}")
+    public Guest updateGuestById(@PathVariable String id, @RequestBody Guest guest) {
+        return guestService.updateGuestById(id, guest);
+    }
+
 }
