b3d0075db45176a4cdee796306d1b678f141c966
ToLa09 <landgraf-tobias@web.de>
2022-10-27 17:28:40 +0200

    feature: add get-request to dwd

---
+++ frontend/src/Weather/WeatherWidget.tsx
@@ -0,0 +1,33 @@
+
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {WeatherModel} from "./WeatherModel";
+
+
+
+export default function WeatherWidget() {
+    axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
+    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+
+    const apiUrl: string = "https://app-prod-ws.warnwetter.de/v30/stationOverviewExtended?stationIds=10637"
+    //const apiUrl: string = "https://rickandmortyapi.com/api/character"
+
+    const [weather, setWeather] = useState<WeatherModel>()
+
+    const getWeather = () => {
+        axios.get(apiUrl, {})
+            .then(response => response.data)
+            .catch(error => console.log(error))
+            //.then(data => setWeather(data))
+            .then(data => console.log(data))
+    }
+
+    useEffect(()=>{
+        getWeather()
+    },[])
+
+    return (
+
+        <div>{}</div>
+    );
+}
