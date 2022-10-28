73ec9d9aaf599e8eb352c0adf62fec69f151ef8a
voelkldennis <voelkldennis@googlemail.com>
2022-10-28 14:47:56 +0200

    open modal in progress

--- frontend/src/Employee/EmployeeCard.tsx
+++ frontend/src/Employee/EmployeeCard.tsx
@@ -5,37 +5,16 @@ import axios from "axios";
 type employeeProps = {
     employee: EmployeeModel,
     deleteEmployee: (id: string) => void,
-    visibleModal: () => void,
+    visibleModal: (id: string) => void,
     getAllEmployees: () => void
 }
 
 export default function EmployeeCard(props: employeeProps) {
 
-    const [newEmployeeName, setEmployeeName] = useState<string>(props.employee.name)
-
-    const handleUpdateEmployeeSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        axios.put("/api/employees/"+props.employee.id, {
-            "id": props.employee.id,
-            "name": newEmployeeName
-        })
-            .catch((e) => console.log("PUT Error: " + e))
-            .then(props.getAllEmployees)
-        setEmployeeName("")
-    }
-
-    const handleNewEmployeeName = (event: ChangeEvent<HTMLInputElement>) => {
-        setEmployeeName(event.target.value)
-    }
-
     return <>
         <li>Name: {props.employee.name} ID: {props.employee.id}
             <button onClick={() => props.deleteEmployee(props.employee.id)}>Delete Employee</button>
-            <button onClick={() => props.visibleModal()}>Edit</button>
+            <button onClick={() => props.visibleModal(props.employee.id)}>Edit</button>
         </li>
-        <form onSubmit={handleUpdateEmployeeSubmit}>
-            <input value={newEmployeeName} onChange={handleNewEmployeeName}/>
-            <button type={"submit"}>Update</button>
-        </form>
     </>
 }
