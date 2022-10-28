cb1c8f41db3c619c7857bd16ffc526ecf33cd609
voelkldennis <voelkldennis@googlemail.com>
2022-10-28 16:37:46 +0200

    change github upper lower case mistake

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Meals from "./Meals/Meals";
-import EmployeeOverview from "./Employee/EmployeeOverview";
+import EmployeeOverview from "./Employeee/EmployeeOverview";
 import PlacementPage from "./Placement/PlacementPage";
 import GuestProfile from "./Guest/GuestProfile";
 import {HashRouter, Route, Routes} from "react-router-dom";
