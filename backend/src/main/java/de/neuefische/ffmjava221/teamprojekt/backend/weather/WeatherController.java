d9f21d71bf63a1d1875ceda27e07e6805bd0e885
christianborn <chrisborn@hotmail.de>
2022-10-28 17:50:39 +0200

    Backend: Add unit test for WeatherService in case request returns null

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherController.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherController.java
@@ -1,10 +1,10 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.weather;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/weather")
@@ -17,11 +17,22 @@ public class WeatherController {
     }
 
     @GetMapping("/today")
-    public WeatherData getWeatherData() {
+    public WeatherData getWeatherToday() {
+        String currentDate = LocalDate.now().toString();
+        int currentHour = Integer.parseInt(java.time.LocalTime.now().toString().split(":")[0]);
         try {
-            return weatherService.fetchWeather();
+            return weatherService.fetchWeatherForecast(currentDate, currentHour);
         } catch (ResponseStatusException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @GetMapping("/{date}")
+    public WeatherData getWeather(@PathVariable String date, @RequestParam int hour) {
+        try {
+            return weatherService.fetchWeatherForecast(date, hour);
+        } catch (ResponseStatusException e) {
+            throw new ResponseStatusException(e.getStatus(), e.getMessage());
         }
     }
 }
