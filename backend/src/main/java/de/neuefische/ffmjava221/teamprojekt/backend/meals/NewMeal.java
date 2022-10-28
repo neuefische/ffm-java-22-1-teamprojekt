1b8349583af15680033bda121d53f54c808f3b6a
christianborn <chrisborn@hotmail.de>
2022-10-28 11:37:01 +0200

    Changes by tobias

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/meals/NewMeal.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/meals/NewMeal.java
@@ -1,3 +1,9 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.meals;
 
-public record NewMeal(String name) {}
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+public record NewMeal(
+        @NotBlank(message = "Please enter a name") @Size(min = 3, message = "String too short") String name) {
+}
