8a75b19deb738dffb2a5294025c5845d42b6b656
anagalit <ana.galit1@gmail.com>
2022-10-28 17:49:31 +0200

    Restored GuestPage

--- frontend/src/Guest/GuestPage.tsx
+++ frontend/src/Guest/GuestPage.tsx
@@ -1,59 +1,28 @@
-import React, {useState} from 'react';
-import "../styles/TaskCard.css"
-import GuestModel{GuestModel} from "../Model/TaskModel";
-import TaskModal from "./TaskModal";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import {AiOutlineArrowRight, AiOutlineArrowLeft} from "react-icons/ai"
-import {RiDeleteBin6Line} from "react-icons/ri"
+import {GuestModel} from "./GuestModel/GuestModel"
+import React from 'react';
+import GuestProfile from "./GuestProfile";
 
+export default function GuestPage() {
+    const [guestList,setGuestList] = useState<GuestModel[]>([])
 
-type TaskCardProps = {
-    task: TaskModel
-    fetchAllTasks: () => void
+    useEffect(() => {
+        fetchAllGuests()
+    }, [])
+
+    const fetchAllGuests = () => {
+        axios.get("/api/guests")
+            .then((response) => response.data)
+            .catch((error) => {
+                console.log('[Error von GET]: =>' + error)
+            })
+            .then((data) => {
+                setGuestList(data)
+            })
+    }
+    return (
+        <GuestProfile guestList={guestList} fetchAllGuests={fetchAllGuests}/>
+    );
 }
 
-export default function TaskCard(props: TaskCardProps) {
-    const [editModal, setEditModal] = useState(false)
-
-    const handleEdit = () => {
-        setEditModal(!editModal)
-    }
-
-    const handleDelete = () => {
-        axios.delete("/api/todo/" + props.task.id)
-            .then(() => {
-                props.fetchAllTasks()
-            })
-            .catch(error => {
-                console.log("[DELETE ERROR:]" + error)
-            })
-    }
-
-    const closeModal = () => {
-        setEditModal(false)
-    }
-
-
-    function handleAdvance() {
-        axios.put("/api/todo/" + props.task.id, {
-            id: props.task.id,
-            description: props.task.description,
-            status: props.task.status === 'OPEN' ? 'IN_PROGRESS' : 'DONE',
-        })
-            .then(response => {
-                props.fetchAllTasks()
-            })
-            .catch(error => console.log(error))
-    }
-
-    function handleBackwards() {
-        axios.put("/api/todo/" + props.task.id, {
-            id: props.task.id,
-            description: props.task.description,
-            status: props.task.status === 'DONE' ? 'IN_PROGRESS' : 'OPEN',
-        })
-            .then(response => {
-                props.fetchAllTasks()
-            })
-            .catch(error => console.log(error))
-    }
