5ee53d2d48690f74c7e5ef83adb830664fd359c2
christianborn <chrisborn@hotmail.de>
2022-10-28 14:56:39 +0200

    Backend: Add integrationtest for fetchWeather

---
+++ backend/src/test/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherIntegrationTest.java
@@ -0,0 +1,35 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.weather;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WeatherIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void fetchWeatherSuccessful() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/today"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.temperature", Matchers.isA(Double.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.temperature").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.condition").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.condition", Matchers.isA(String.class)));
+    }
+}
