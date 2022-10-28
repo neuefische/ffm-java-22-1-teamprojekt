6239a2ba1053e1f63963b4032392e7a95e3bf276
christianborn <chrisborn@hotmail.de>
2022-10-28 16:48:34 +0200

    Backend: Add rendering of weather data

--- frontend/src/Weather/WeatherModel.ts
+++ frontend/src/Weather/WeatherModel.ts
@@ -1,9 +1,4 @@
 export type WeatherModel = {
-    days: {
-        dayDate: string
-        temperatureMin: number
-        temperatureMax: number
-        windSpeed: number
-        sunshine: number
-    }[]
+    condition: string,
+    temperature: number,
 }
