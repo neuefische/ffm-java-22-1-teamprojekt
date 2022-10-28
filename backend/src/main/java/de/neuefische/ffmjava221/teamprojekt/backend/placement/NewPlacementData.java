1db8cc0f95476f480524b9428c7a313995bacfb0
mykola <bobirets@gmail.com>
2022-10-28 12:47:45 +0200

    add Validation Put&Post Placement

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/NewPlacementData.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/placement/NewPlacementData.java
@@ -1,9 +1,15 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.placement;
 
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
-
-
-
-public record NewPlacementData (int placementNr, int totalSeats) {
+public record NewPlacementData(
+        @NotNull(message = "PlacementNr must not be Null")
+        int placementNr,
+        @NotNull
+        @Min(value = 2, message = "Seats must be between 2 and 15")
+        @Max(value=15, message = "Seats must be between 2 and 15")
+        int totalSeats) {
 }
