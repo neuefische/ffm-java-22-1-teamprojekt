13d3aff644251a0c4890d82d66c9b8a81fa47040
anagalit <ana.galit1@gmail.com>
2022-10-28 16:47:04 +0200

    Updated GuestPage

---
+++ frontend/src/Guest/GuestPage.tsx
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {GuestModel} from "./GuestModel/GuestModel"
+import React from 'react';
+import GuestProfile from "./GuestProfile";
+
+export default function GuestPage() {
+    const [guestList,setGuestList] = useState<GuestModel[]>([])
+
+    useEffect(() => {
+        fetchAllGuests()
+    }, [])
+
+    const fetchAllGuests = () => {
+        axios.get("/api/guests")
+            .then((response) => response.data)
+            .catch((error) => {
+                console.log('[Error von GET]: =>' + error)
+            })
+            .then((data) => {
+                setGuestList(data)
+            })
+    }
+
+    return (
+        <GuestProfile guestList={guestList} fetchAllGuests={fetchAllGuests}/>
+    );
+}
+
+
+
