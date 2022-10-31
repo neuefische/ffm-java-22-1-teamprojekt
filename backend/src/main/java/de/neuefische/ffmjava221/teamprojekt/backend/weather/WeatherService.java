d16a14772c08f7c479eaf2b26aaecfe22c877847
ToLa09 <landgraf-tobias@web.de>
2022-10-31 12:44:14 +0100

    Changes by christianborn

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherService.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherService.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
 
 @Service
 public class WeatherService {
