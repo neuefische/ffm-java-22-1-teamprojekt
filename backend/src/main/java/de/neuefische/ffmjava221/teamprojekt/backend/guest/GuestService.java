39418337e294b93b7eabc913ad0c99c9c1cef0a6
Zeshan Shahid <z.shahid@hotmail.de>
2022-10-31 12:36:44 +0100

    add delteMapping and implement in service and repo

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/GuestService.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/GuestService.java
@@ -37,4 +37,8 @@ public class GuestService {
         }
         throw new NoSuchElementException("No guest was found with this id");
     }
+
+    public void deleteGuestById(String id) {
+        guestRepo.deleteGuestById(id);
+    }
 }
