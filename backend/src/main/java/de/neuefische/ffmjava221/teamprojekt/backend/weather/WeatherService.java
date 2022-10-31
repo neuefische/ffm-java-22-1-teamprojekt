781dcb737b130db66ecc0421cff79097513f22ca
ToLa09 <landgraf-tobias@web.de>
2022-10-31 12:44:36 +0100

    Changes by ira

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherService.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/weather/WeatherService.java
@@ -1,7 +1,7 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.weather;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
