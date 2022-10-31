d7b11a8128bbe54eaa5dcb519cc91bfd0399cd68
Zeshan Shahid <z.shahid@hotmail.de>
2022-10-31 14:39:03 +0100

    add delete button in frontend and integration test in backend

--- frontend/src/Guest/GuestCard.tsx
+++ frontend/src/Guest/GuestCard.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {GuestModel} from "./GuestModel/GuestModel";
 import GuestModal from "./GuestModal/GuestModal";
+import axios from "axios";
 
 type GuestCardProps = {
     guest: GuestModel;
@@ -8,7 +9,8 @@ type GuestCardProps = {
 }
 
 export default function GuestCard(props: GuestCardProps) {
-    const [editModal,setEditModal] = useState(false)
+    const [editModal, setEditModal] = useState(false)
+    const [messageStatus, setMessageStatus] = useState('')
 
     const handleEdit = () => {
         setEditModal(!editModal)
@@ -18,16 +20,36 @@ export default function GuestCard(props: GuestCardProps) {
         setEditModal(false)
     }
 
+
+    const deleteGuest = () => {
+        axios.delete("/api/guests/" + props.guest.id)
+            .then((response) => response.status)
+            .catch((error) => {
+                console.log('[Error von GET]: =>' + error)
+                if (error.status === 404) setMessageStatus('Error: Delete not successful!!')
+            })
+            .then((status) => {
+                if (status === 200) {
+                    setMessageStatus('Guest ' + props.guest.firstName+" "+props.guest.lastName + ' successfully deleted.');
+                }
+            }).then(() => setTimeout(() => props.fetchAllGuests(), 2000))
+    }
+
+    // GuestCard
+
     return (
         <>
-            {editModal && <GuestModal closeModal={closeModal} guest={props.guest} fetchAllTasks={props.fetchAllGuests}/>}
+            {editModal &&
+                <GuestModal closeModal={closeModal} guest={props.guest} fetchAllTasks={props.fetchAllGuests}/>}
+            {messageStatus && <p>{messageStatus}</p>}
             <section>
-            <h3>Guest {props.guest.lastName}</h3>
-        <div>
-        <p> Firstname: {props.guest.firstName}</p>
-        <p> Lastname: {props.guest.lastName}</p>
-        <p> email: {props.guest.email}</p>
-            <button onClick={handleEdit}>Edit Guest</button>
+                <h3>Guest {props.guest.lastName}</h3>
+                <div>
+                    <p> Firstname: {props.guest.firstName}</p>
+                    <p> Lastname: {props.guest.lastName}</p>
+                    <p> email: {props.guest.email}</p>
+                    <button onClick={handleEdit}>Edit Guest</button>
+                    <button onClick={deleteGuest}>delete</button>
 
         </div>
             </section>
