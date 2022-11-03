db074b738dbef6084c73cf0f1be1aeac03450591
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-03 12:31:00 +0100

    change regex for email

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/Guest.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/Guest.java
@@ -1,6 +1,5 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.guest;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -9,7 +8,7 @@ public record Guest(
         String firstName,
         @NotBlank
         String lastName,
-        @Email
+        @Pattern(regexp ="/\\S+@\\S+\\.\\S+/",message = "Email should be a valid address")
         String email,
         @Pattern(regexp = "^(?=[^A-Z]*+[A-Z])(?=[^a-z]*+[a-z])(?=\\D*+\\d)(?=[^#?!@$ %^&*-]*+[#?!@$ %^&*-]).{8,}$",message = "Password must have minimum eight characters, at least one letter and one number!")
         String password,
