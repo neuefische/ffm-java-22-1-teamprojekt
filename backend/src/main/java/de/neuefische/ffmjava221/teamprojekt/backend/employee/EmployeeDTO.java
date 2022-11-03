0b38befaea99ab6c29d91b372547202b029791e0
chris_yooo <hhxtreams@gmail.com>
2022-11-03 16:44:56 +0100

    verify mocks in service test + EmployeeDTO change

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeDTO.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/employee/EmployeeDTO.java
@@ -1,7 +1,15 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.employee;
 
-public record UpdateEmployee(
+import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.NotBlank;
+
+public record EmployeeDTO(
+        @Id
+        String id,
+
+        @NotBlank
+        String name
 )
  {
     public static Employee toUpdateTimeStamp(String id, String name, String regTimeStamp) {
