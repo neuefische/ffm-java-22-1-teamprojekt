40b7c9ebda1507ebe0d28cf44891eba16f1d88ab
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-03 12:34:02 +0100

    change validation for email

--- backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/Guest.java
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/Guest.java
@@ -1,5 +1,6 @@
 package de.neuefische.ffmjava221.teamprojekt.backend.guest;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -8,7 +9,7 @@ public record Guest(
         String firstName,
         @NotBlank
         String lastName,
-        @Pattern(regexp ="/\\S+@\\S+\\.\\S+/",message = "Email should be a valid address")
+        @Email
         String email,
         @Pattern(regexp = "^(?=[^A-Z]*+[A-Z])(?=[^a-z]*+[a-z])(?=\\D*+\\d)(?=[^#?!@$ %^&*-]*+[#?!@$ %^&*-]).{8,}$",message = "Password must have minimum eight characters, at least one letter and one number!")
         String password,
