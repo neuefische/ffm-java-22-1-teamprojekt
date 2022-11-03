cfc486989a4e86f756b2e563032d21423203c092
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-03 12:45:18 +0100

    re re re

---
+++ backend/src/main/java/de/neuefische/ffmjava221/teamprojekt/backend/guest/GuestRepository.java
@@ -0,0 +1,6 @@
+package de.neuefische.ffmjava221.teamprojekt.backend.guest;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface GuestRepository extends MongoRepository<Guest, String> {
+}
