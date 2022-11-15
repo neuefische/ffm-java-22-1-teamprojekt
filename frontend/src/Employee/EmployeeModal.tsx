8a4348cfd71f6bd7ac4bff0878086597067626ac
chris_yooo <hhxtreams@gmail.com>
2022-11-15 19:26:48 +0100

    add css reset add color variables add styled components insert icons with iconify add style to home add style to employees

--- frontend/src/Employee/EmployeeModal.tsx
+++ frontend/src/Employee/EmployeeModal.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, FormEvent, useState} from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 type updateEmployeeProps = {
     closeModal: () => void
@@ -8,7 +9,7 @@ type updateEmployeeProps = {
     getAllEmployees: () => void
 }
 
-export default function EmployeeModal(props:updateEmployeeProps){
+export default function EmployeeModal(props: updateEmployeeProps) {
 
     const [newEmployeeName, setEmployeeName] = useState<string>()
 
@@ -28,12 +29,74 @@ export default function EmployeeModal(props:updateEmployeeProps){
     }
 
     return (
-        <div>
-            <form onSubmit={handleUpdateEmployeeSubmit}>
-                <input type={"text"} value={newEmployeeName} onChange={handleNewEmployeeName}/>
-                <button type={"submit"}>Update</button>
-            </form>
-            <button onClick={props.closeModal}>Close</button>
-        </div>
+        <StyledArticle>
+            <StyledName>Please insert new name:</StyledName>
+            <StyledForm onSubmit={handleUpdateEmployeeSubmit}>
+                <StyledInput type={"text"} value={newEmployeeName} onChange={handleNewEmployeeName}/>
+                <StyledButton type={"submit"}>Update</StyledButton>
+            </StyledForm>
+            <StyledDiv>
+                <StyledButton onClick={props.closeModal}>Close</StyledButton>
+            </StyledDiv>
+        </StyledArticle>
     );
 }
+
+const StyledArticle = styled.article`
+  position: absolute;
+  flex-direction: column;
+  padding: 50px;
+  background-color: var(--color-lightgrey);
+  border: 1px solid var(--color-black);
+  border-radius: 1pc;
+  box-shadow: 0 .0625rem .5rem 0 rgba(0, 0, 0, .04), 0 .0625rem .3125rem 0 rgba(0, 0, 0, .04);
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledDiv = styled.div`
+  position: relative;
+  top: 40px;
+  left: 195px;
+`
+
+const StyledName = styled.p`
+  position: relative;
+  top: -35px;
+  left: -25px;
+  margin-bottom: 5px;
+  padding: 4px;
+  font-size: 1.1rem;
+`
+
+const StyledInput = styled.input`
+  margin: 3px;
+  padding: 3px;
+  border-radius: 5px;
+  box-shadow: 0 .0625rem .5rem 0 rgba(0, 0, 0, .04), 0 .0625rem .3125rem 0 rgba(0, 0, 0, .04);
+`;
+
+const StyledButton = styled.button`
+  padding: 8px 10px;
+  font-size: 0.8rem;
+  cursor: pointer;
+  margin: 3px;
+  background-color: var(--color-button-background);
+  color: var(--color-text);
+  border: none;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: var(--color-button-hover);
+  }
+
+  &:active {
+    background-color: var(--color-button-active);
+  }
+`;
--- frontend/src/NavBar/Navigation.tsx
+++ frontend/src/NavBar/Navigation.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import { Icon } from '@iconify/react';
+import styled from "styled-components";
 
 export default function Navigation() {
     return (
         <nav>
             <NavLink to="/GuestProfile">
-                <button>GuestList</button>
+                <StyledButton>
+                    <Icon icon="mdi:user" /> GuestList
+                </StyledButton>
             </NavLink>
             <NavLink to="/RegisterForm">
-                <button>Registration</button>
+                <StyledButton>
+                    <Icon icon="mdi:register" /> Registration
+                </StyledButton>
             </NavLink>
             <NavLink to="/Meals">
-                <button>Meals</button>
-            </NavLink>
-            <NavLink to="/Employees">
-                <button>Employees</button>
+                <StyledButton>
+                    <Icon icon="healthicons:hot-meal" /> Meals
+                </StyledButton>
             </NavLink>
             <NavLink to="/Placements">
-                <button>Placements</button>
+                <StyledButton>
+                    <Icon icon="material-symbols:table-bar" /> Placements
+                </StyledButton>
+            </NavLink>
+            <NavLink to="/Employees">
+                <StyledButton>
+                    <Icon icon="clarity:employee-group-solid" /> Employees
+                </StyledButton>
             </NavLink>
         </nav>
     );
 }
+const StyledButton = styled.button`
+  padding: 8px 10px;
+  font-size: 1rem;
+  cursor: pointer;
+  margin: 3px;
+  transition-duration: 0.4s;
+  background-color: var(--color-button-background);
+  color: var(--color-text);
+  border: none;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: var(--color-button-hover);
+  }
+
+  &:active {
+    background-color: var(--color-button-active);
+  }
+`;
